ac7c6a2c5ea8358f1fdfcee09c2c7ecddac87a59
davidoberst <juandavidbustosperez82@gmail.com>
2026-03-13 08:20:36 -0500

    add headers and error managment, i guess

--- dominic.py
+++ dominic.py
@@ -7,8 +7,6 @@ print("")
 
 print(pyfiglet.figlet_format(text="dominic",font="smblock"),end="")
 print("domain status check")
-
-
 parser = argparse.ArgumentParser()
 parser.add_argument("-d", "--domain", required=True, help="wordlist path") 
 args = parser.parse_args()
@@ -17,6 +15,9 @@ args = parser.parse_args()
 if not os.path.exists(args.domain):
     print(f"[!] '{args.domain}' not found.")
     exit()
+headers = {
+"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36"
+}
 active_urls = []
 nonactive = []
 with open(args.domain, "r") as domains:
@@ -27,16 +28,21 @@ with open(args.domain, "r") as domains:
         url = line.strip()
         if not url: continue       
         if not url.startswith("http"):
-            url = f"http://{url}"
+            url = f"https://{url}"
         try:
-         response = requests.get(url, timeout=5)
+         response = requests.get(url, timeout=5, headers=headers)
          if response.ok:
             active_urls.append(f"{url} - {response.status_code}")
          else:
-            pass
-        except requests.exceptions.RequestException:
-           nonactive.append(f"{url} - {response.status_code}")
-        
+            nonactive.append(f"{url} - {response.status_code}")
+        except:
+           nonactive.append(f"{url} - FAILED/TIMEOUT")
+#results
+for x in active_urls:
+   print(x)
+for y in nonactive:
+   print(y)
+
            
 
 
